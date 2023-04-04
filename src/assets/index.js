56b8b7f2d47c03df3dd658bf0cb703a82d71db90
MauJho <maurieljng@gmail.com>
2023-04-04 11:56:54 -0600

    First commit

---
+++ src/assets/index.js
@@ -0,0 +1,68 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import csharp from "./csharp.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import asp from "./asp.png";
+import sqlserver from "./sqlserver.svg";
+import MMM from "./MMM.png";
+import HP from "./HP.png";
+import StarTek from "./startek.png";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import TRI from "./TRI.png";
+
+import WebApi from "./WebApi.png";
+import mobileBike from "./mobileBike.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  csharp,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  HP,
+  MMM,
+  StarTek,
+  mobileBike,
+  tripguide,
+  asp,
+  sqlserver,
+  TRI,
+  WebApi,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,257 @@
+import { 
+  csharp,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  TRI,
+  MMM,
+  StarTek,
+  HP,
+  WebApi,
+  mobileBike,
+  tripguide,
+  threejs,
+  asp,
+  sqlserver,
+
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "C-Sharp Developer",
+    icon: csharp,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Developer",
+    icon: reactjs,
+  },
+];
+
+const technologies = [
+  {
+    name: "C-Sharp",
+    icon: csharp,
+  },
+  {
+    name: "ASP.NET",
+    icon: asp,
+  },
+  {
+    name: "HTML",
+    icon: html,
+  },
+  {
+    name: "CSS",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  }, 
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "SQLServer",
+    icon: sqlserver,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  
+
+];
+
+const experiences = [
+  {
+    title: "Solution Specialists",
+    company_name: "3M",
+    icon: MMM,
+    iconBg: "#383E56",
+    date: "July 2018 - Current",
+    points: [
+      "Developing and maintaining Automations, Reports and Applications using Visual Basic, Power Automate, Power Apps and Power BI and other related technologies.",
+      "Collaborating with cross-functional teams and other developers to create high-quality products.",
+      "OTC Data Readiness:",
+      " Evaluating the Customers Order Readiness throughout the readiness and deployment process. Identify data maping",
+      "and transformation logic impacting COR. Building and Analyzing COR Readiness dasboard and facilitating resolution of identified errors.",
+      "Point Of Sales:",
+      "Search Materials in SAP and the 3M official pages, such as: Product Center, Catalog, GGSM, History and Power BI reports.",
+      "Participate in the transition of legacy system to SAP. Be part of the POS SAP training and floor support. Build Power BI and Macros." 
+      
+    ],
+  },
+  {
+    title: "Accounts Recivable Representative",
+    company_name: "Thomson Reuters",
+    icon: TRI,
+    iconBg: "#E6DEDD",
+    date: "2017 - 2018",
+    points: [
+      "Outbound and Incoming calls to collect past due invoices from a specific portfolio assinged individually but getting goals as a team, helping eachother  when the assignment must be handled carefully due to the complex of each task.",
+      "Replying emails to customers regarding to receipts, payments or extra charges.",
+    ],
+  },
+  {
+    title: "Onsite Coordination",
+    company_name: "HP",
+    icon: HP,
+    iconBg: "#383E56",
+    date: "2014 - 2017",
+    points: [
+      "Support engineers in requesting reschedules, revisits or cases reassignments.",
+      "Be part of trasaction to legacy system OPT to SalesForce and assist the team to surf properly in the new system.",
+    ],
+  },
+  {
+    title: "Technical Support Agent",
+    company_name: "StartTek",
+    icon: StarTek,
+    iconBg: "#E6DEDD",
+    date: "2013 - 2014",
+    points: [
+      "Providing Technical Support and Customer Service to the services of Cable, Internet and IP phone.",
+      
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Web API",
+    description:
+      "This is a simple CRUD application that use Swagger UI, JWT authentication and HTTP Methods. ",
+    tags: [
+      {
+        name: "C-Sharp",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "ASP.NET",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Entity Framework",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: WebApi,
+    source_code_link: "https://github.com/MauJho/ApiWeb",
+  },
+  {
+    name: "Mobile Bike Reparis",
+    description:
+      "using HTML and CSS technologies I created a Web page for a Bike repairs. ",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+    ],
+    image: mobileBike,
+    source_code_link: "https://github.com/MauJho/Mobile-Bike-Repair",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
